c2819d2bc4c18d88025c697bcbeb9400f393c4f6
ad-mitium <99030354+ad-mitium@users.noreply.github.com>
2023-05-23 16:25:28 -0400

    Added fourth camera

--- config/cameras.py
+++ config/cameras.py
@@ -1,7 +1,8 @@
 camera_list = {
-    '1':'http://192.168.0.124:81/stream' ,
-    '2':'http://192.168.0.125:81/stream' ,
-    '3':'http://192.168.0.126:81/stream'
+    '1':'http://192.168.0.123:81/stream' ,
+    '2':'http://192.168.0.124:81/stream' ,
+    '3':'http://192.168.0.125:81/stream' ,
+    '4':'http://192.168.0.126:81/stream'
 }
 
 # Only change these if your device supports it
