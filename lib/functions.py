73c576b7053e1aaf0200553a37baa55dd2f441e0
ad-mitium <99030354+ad-mitium@users.noreply.github.com>
2025-03-02 13:39:11 -0500

    Update print with cam_session data

--- lib/functions.py
+++ lib/functions.py
@@ -603,12 +603,12 @@ def get_multi_frames(cam_id_1,cam_id_2,cam_id_3,cam_id_4,stop_capture=False,show
             cam_online_status[int(cam_id)]=session['online_status']
             # session.update(fs_size=sess_defaults[cam_id][2])        # Store original frame size setting
             set_frame_size(cam_id,'11')
-            write_session_data(session['camera_id'], session['ae_level'], session['bpc'], session['fs_size'], session['white_balance'], session['flip'], show_debug_info)
-            print ("Cam: ",cam_id," ae:", session['ae_level']," wb:", session['white_balance']," bpc:", session['bpc'],"flip mode:", session['flip'], end=" ] ")
+            # write_session_data(session['camera_id'], session['ae_level'], session['bpc'], session['fs_size'], session['white_balance'], session['flip'], show_debug_info)
+            print ("Cam: ",cam_id," ae:", cam_session[str(cam_id)]['ae_level']," wb:", cam_session[str(cam_id)]['white_balance']," bpc:", cam_session[str(cam_id)]['bpc'],"flip mode:", cam_session[str(cam_id)]['flip'], end=" ] ")
         if show_debug_info == 'DEBUG': 
             print ('\nDEBUG:   Frame size reset for Cam ID: ',cam_id)
     print ("")      # Send newline after all camera session info is printed
-    print (sess_defaults)
+    # print (sess_defaults)
     if show_debug_info == 'DEBUG': 
         print (cam_online_status)
 
