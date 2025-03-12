8471206c1097d4f01b56f7dd033e8683f26e700c
ad-mitium <99030354+ad-mitium@users.noreply.github.com>
2025-03-12 09:49:55 -0400

    Shift INFO dialogue

--- lib/functions.py
+++ lib/functions.py
@@ -48,8 +48,8 @@ def initialize_cams(show_debug_info=False):
             
 
     for cam_id in sess_defaults.keys():
-        print ('INFO:    Initializing camera', cam_id)
         if not cam_id == '0':       # Don't assign '0', as it is not being written
+            print ('INFO:    Initializing camera', cam_id)
             session['camera_id']=cam_id     # Change camera ID or you'll overwrite the same one over and over
             session.update(ae_level=sess_defaults[cam_id][0],bpc=sess_defaults[cam_id][1],fs_size=sess_defaults[cam_id][2],
                 white_balance=sess_defaults[cam_id][3],flip=sess_defaults[cam_id][4],ae_compensation=sess_defaults[cam_id][5],
